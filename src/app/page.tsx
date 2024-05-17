adf8a7ac28c788bcc086dbe6c09493127e073e8b
anirudh <anirudh@kindtech.co.in>
2024-05-17 15:34:19 +0530

    UI done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
-import { Box, Center, Input, Text, Button } from "@chakra-ui/react";
+import { Box, Center, Input, Text, Button, Flex } from "@chakra-ui/react";
 import Chips from "../components/chips/page";
-
+import Feeds from "@/components/feeds/page";
 const page = () => {
+  const arr = ["HELLO", "HELLO", "HELLO", "HELLO", "HELLO"];
   return (
     <Box w="70%">
       <Text align="center">Feed</Text>
       <Center>
         <Box w="80%">
-          <Input placeholder="large size" size="lg" />
-          <Box w="100%" textAlign="right">
-            <Button type="submit">Post</Button>
+          <Input type="string" placeholder="large size" size="lg" />
+          <Box mt="10px" w="100%" textAlign="right">
+            <Button colorScheme="teal" type="submit">
+              Post
+            </Button>
           </Box>
         </Box>
       </Center>
       <Chips />
+      {arr.map((ele) => (
+        <Flex justifyContent="center">
+          <Feeds date="thu may 16 2024" content="HELLO" />
+        </Flex>
+      ))}
     </Box>
   );
 };
---
+++ src/components/feeds/page.tsx
@@ -0,0 +1,35 @@
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+interface props {
+  content: string;
+  date: string;
+}
+
+const Feeds: React.FC<props> = ({content,date}:props) => {
+  // const content: string = "HELLO";
+  // const date: string = "thu may 16 2024";
+  return (
+    <>
+      <Box mt='10'
+        boxShadow="0px 4px 6px rgba(0.2,0.2,0.2,0.2)"
+        w="80%"
+        p="4"
+        borderRadius="xl"
+        border="1px solid grey"
+      >
+        <Text marginBottom="10px" as="b">
+          {content}
+        </Text>
+        <Flex mt="30px" justifyContent="space-between" alignItems="center">
+          <Text color="gray.500">posted on {date}</Text>
+          <Button size="sm" colorScheme="teal" type="submit">
+            delete
+          </Button>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default Feeds;
