bac888c62c8363acba5bf83d14ca4ef026d7d46c
anirudh <anirudh@kindtech.co.in>
2024-05-20 15:41:57 +0530

    compact

--- src/components/rightbar/page.tsx
+++ src/components/rightbar/page.tsx
@@ -1,41 +1,21 @@
-import { daysInWeek, data } from "@/constants/data";
+"use client";
+import { daysInWeek } from "@/constants/data";
+import { useAppSelector } from "@/redux/store";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const RightBar: React.FC = () => {
-  // Initialize an object to store the count of posts for each day
-  const postCounts: { [key: string]: number } = {
-    sun: 0,
-    mon: 0,
-    tue: 0,
-    wed: 0,
-    thu: 0,
-    fri: 0,
-    sat: 0,
+  const dataState = useAppSelector((state) => state.feedSlice);
+  const postCounts: { [key: string]: number } = {sun: 0,mon: 0,tue: 0,wed: 0,thu: 0,fri: 0,sat: 0,
   };
-
-  // Parse the posted_on dates and count the posts for each day
-  data.forEach((post) => {
+  dataState.forEach((post) => {
     const date = new Date(post.posted_on);
     const day = daysInWeek[date.getDay()];
     postCounts[day]++;
   });
 
   return (
-    <Flex
-      alignItems="center"
-      justifyContent="center"
-      bg="teal"
-      position="fixed"
-      top={0}
-      right={0}
-      bottom={0}
-      borderRight="1px solid grey"
-      h="100vh"
-      width="200px"
-      zIndex={10}
-      overflowY="auto"
-    >
+    <Flex alignItems="center" justifyContent="center" bg="teal" position="fixed" top={0} right={0} bottom={0} borderRight="1px solid grey" h="100vh" width="200px" zIndex={10} overflowY="auto" >
       <Box>
         <Text color="white" mb="10">
           Weekly
