f2fb737e45b28179110760e59ce128c10c8d8b1d
anirudh <anirudh@kindtech.co.in>
2024-05-20 15:32:35 +0530

    feeds

--- src/components/rightbar/page.tsx
+++ src/components/rightbar/page.tsx
@@ -1,37 +1,52 @@
-import { daysInWeek } from "@/constants/data";
+import { daysInWeek, data } from "@/constants/data";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const RightBar: React.FC = () => {
-  const _Points: Array<string> = [...daysInWeek];
+  // Initialize an object to store the count of posts for each day
+  const postCounts: { [key: string]: number } = {
+    sun: 0,
+    mon: 0,
+    tue: 0,
+    wed: 0,
+    thu: 0,
+    fri: 0,
+    sat: 0,
+  };
+
+  // Parse the posted_on dates and count the posts for each day
+  data.forEach((post) => {
+    const date = new Date(post.posted_on);
+    const day = daysInWeek[date.getDay()];
+    postCounts[day]++;
+  });
+
   return (
-    <>
-      <Flex
-        alignItems="center"
-        justifyContent="center"
-        bg="teal"
-        position="fixed"
-        top={0}
-        right={0}
-        bottom={0}
-        borderRight="1px solid grey"
-        h="100vh"
-        width="200px"
-        zIndex={10}
-        overflowY="auto"
-      >
-        <Box>
-          <Text color="white" mb="10">
-            Weekly
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      bg="teal"
+      position="fixed"
+      top={0}
+      right={0}
+      bottom={0}
+      borderRight="1px solid grey"
+      h="100vh"
+      width="200px"
+      zIndex={10}
+      overflowY="auto"
+    >
+      <Box>
+        <Text color="white" mb="10">
+          Weekly
+        </Text>
+        {daysInWeek.map((day, index) => (
+          <Text color="white" align="center" cursor="pointer" key={index}>
+            {day} | {postCounts[day]}
           </Text>
-          {_Points.map((ele: string, ind: number) => (
-            <Text color="white" align="center" cursor="pointer" key={ind}>
-              {ele}
-            </Text>
-          ))}
-        </Box>
-      </Flex>
-    </>
+        ))}
+      </Box>
+    </Flex>
   );
 };
 
