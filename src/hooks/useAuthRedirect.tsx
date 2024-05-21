ff3593d9827827786efdead404b43a0fd3ba0962
anirudh <anirudh@kindtech.co.in>
2024-05-21 13:46:35 +0530

    added hooks

---
+++ src/hooks/useAuthRedirect.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAppSelector } from "@/redux/store";
+
+const useAuthRedirect = (
+  redirectPathIfUnauth: string,
+  options: string[] | null,
+  currentPath: string | null
+): boolean | undefined => {
+  const userState = useAppSelector((state) => state.authSlice.isAuth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!userState) {
+      router.push(redirectPathIfUnauth);
+    } else if (options && !options.includes(currentPath || "")) {
+      router.push(options[0]);
+    }
+  }, [userState, router, currentPath, options, redirectPathIfUnauth]);
+
+  return userState;
+};
+
+export default useAuthRedirect;
---
+++ src/hooks/useValidPath.tsx
@@ -0,0 +1,13 @@
+const validatePath = (path: string, options: string[]): { lastPathname: string, secondLastPathname: string, isValid: boolean } => {
+    const PathArr = path.split("/");
+    const lastPathname = PathArr[PathArr.length - 1];
+    const secondLastPathname = PathArr[PathArr.length - 2];
+    return {
+      lastPathname,
+      secondLastPathname,
+      isValid: options.includes(secondLastPathname) && !isNaN(Number(lastPathname)) && Number(lastPathname) >= 1 && Number(lastPathname) <= options.length
+    };
+  };
+  
+  export default validatePath;
+  
