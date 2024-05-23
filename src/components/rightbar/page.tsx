3728fa283356ef23de02275abf0d4ce92287331e
anirudh <anirudh@kindtech.co.in>
2024-05-23 14:03:20 +0530

    rightbar style prop removed

--- src/components/rightbar/page.tsx
+++ src/components/rightbar/page.tsx
@@ -5,13 +5,21 @@ import { useAppSelector } from "@/redux/store";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import { useDispatch } from "react-redux";
+import "./page.css";
 
 const RightBar: React.FC = () => {
   const dispatch = useDispatch();
   const specificDay = useAppSelector((state) => state.specificDaySlice);
   const dataState = useAppSelector((state) => state.feedSlice);
 
-  const postCounts: { [key: string]: number } = {  sun: 0, mon: 0, tue: 0, wed: 0, thu: 0, fri: 0, sat: 0,
+  const postCounts: { [key: string]: number } = {
+    sun: 0,
+    mon: 0,
+    tue: 0,
+    wed: 0,
+    thu: 0,
+    fri: 0,
+    sat: 0,
   };
 
   dataState.forEach((post) => {
@@ -29,20 +37,7 @@ const RightBar: React.FC = () => {
   };
 
   return (
-    <Flex
-      alignItems="center"
-      justifyContent="center"
-      bg="teal"
-      position="fixed"
-      top={0}
-      right={0}
-      bottom={0}
-      borderRight="1px solid grey"
-      h="100vh"
-      width="200px"
-      zIndex={10}
-      overflowY="auto"
-    >
+    <Flex className="right-bar">
       <Box>
         <Text color="white" mb="10">
           Weekly
@@ -51,11 +46,9 @@ const RightBar: React.FC = () => {
           <Text
             fontWeight={specificDay.includes(day) ? "900" : "normal"}
             onClick={() => {
-              if (specificDay.includes(day)) {
-                removeSpecificDay(day);
-              } else {
-                handleDayAdd(day);
-              }
+              specificDay.includes(day)
+                ? removeSpecificDay(day)
+                : handleDayAdd(day);
             }}
             color="white"
             align="center"
